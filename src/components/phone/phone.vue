c162d1b0e8cc1fb42f4b36cda464af53f3e5c9ab
Esra Koçak <esrakocak470@gmail.com>
2022-10-02 12:28:22 +0300

    phone

---
+++ src/components/phone/phone.vue
@@ -0,0 +1,31 @@
+<script setup>
+	const emits = defineEmits(["update: phoneNumber", "isValid"]);
+	
+	const inputHandler = (e) => {
+		const isValid =
+			/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+				e.target.value
+			);
+	
+		if (isValid) {
+			emits("update: phoneNumber", e.target.value);
+			return;phoneNumber
+		}
+	
+		emits("isValid", false);
+		return;
+	};
+	
+	const props = defineProps(["phoneNumber"]);
+	</script>
+	
+	<template>
+		<div>
+			<input
+				:type="type"
+				id="phoneNumber"
+				:value="props.phone"
+				@input="inputHandler"
+			/>
+		</div>
+	</template>
