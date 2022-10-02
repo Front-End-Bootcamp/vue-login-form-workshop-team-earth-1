3e2f4eac2d131fb4d65eae1813ba5e88d0efe7b1
ilkerdurmaz <ilker.prime@gmail.com>
2022-10-02 12:06:28 +0300

    password input component is configured

---
+++ src/components/PasswordInput.vue
@@ -0,0 +1,31 @@
+<script setup>
+const emits = defineEmits(["update:password", "isValid"]);
+
+const inputHandler = (e) => {
+	const isValid =
+		/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+			e.target.value
+		);
+
+	if (isValid) {
+		emits("update:password", e.target.value);
+		return;
+	}
+
+	emits("isValid", false);
+	return;
+};
+
+const props = defineProps(["type", "password"]);
+</script>
+
+<template>
+	<div>
+		<input
+			:type="type"
+			id="passwordInput"
+			:value="props.password"
+			@input="inputHandler"
+		/>
+	</div>
+</template>
